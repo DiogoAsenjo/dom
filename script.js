6e1113f759c7a4f6a88af1b20ea4746ad25436e2
Diogo Asenjo <diogoasenjo@outlook.com>
2024-10-24 11:45:03 -0300

    Manipulando valores com o DOM

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+//Aula de seleção de elementos
 const h1 = document.querySelector("h1"); //Selecionando pela tag.
 const classe = document.querySelector(".classe"); //Selecionando classe precisa de .
 const id = document.querySelector("#id"); //Selecionando id precisa de #
@@ -22,3 +22,31 @@ allPs.forEach((element, index) => {
     element.style.background = "red";
   }
 });
+
+//Alterando valores
+const pQueOsValoresSeraoAlterados = document.querySelector("#alterandoValor");
+
+//Adicionando elemento html com inner hmtl
+const divParaAlterar = document.querySelector("#criandoCoisas");
+divParaAlterar.innerHTML = `${divParaAlterar.innerHTML} <strong>Adicionando texto strong via innerHTML no JS</stong`;
+
+//Adicionando elemento com createElement
+const array = [1, 2, 3];
+const elementUl = document.createElement("ul");
+array.forEach((element) => {
+  const elementLi = document.createElement("li");
+  elementLi.setAttribute("id", `lista${element}`); //Setando atributo.
+  elementLi.innerText = element;
+  elementUl.appendChild(elementLi);
+});
+
+divParaAlterar.appendChild(elementUl);
+
+//Removendo elemento
+const elementoLi = document.querySelector("#lista1");
+console.log(elementoLi);
+elementoLi.remove();
+
+//Manipulando input. Usa value ao invés de innerText
+const inputNome = document.querySelector("#nome");
+inputNome.value = "Valor Padrão";
