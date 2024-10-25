9119fb494be88a1ed139275275db1c3d9706409c
Diogo Asenjo <diogoasenjo@outlook.com>
2024-10-25 09:30:40 -0300

    Finalizando os status de DOM

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# dom
-Reaprendendo e vendo detalhes do DOM.
+# DOM
+
+Visando aprimorar meus conhecimentos em front-end, resolvi dar um passo para trás e relembrar alguns elementos e característica do DOM
--- script.js
+++ script.js
@@ -50,3 +50,62 @@ elementoLi.remove();
 //Manipulando input. Usa value ao invés de innerText
 const inputNome = document.querySelector("#nome");
 inputNome.value = "Valor Padrão";
+
+//Manipulando style
+h1.style.color = "red";
+
+//Puxando elemento pai
+console.log(h1.parentElement);
+
+//Puxando elemento filho
+console.log(elementUl.childNodes);
+
+//Eventos
+const botao1 = document.querySelector("#botao1");
+botao1.onclick = function alerta() {
+  alert("666");
+};
+
+//Event delegation
+const menu = document.querySelector(".menu");
+menu.addEventListener("click", (event) => {
+  const { target } = event;
+  switch (target.getAttribute("class")) {
+    case "home":
+      console.log("home");
+      break;
+    case "sobre":
+      console.log("sobre");
+      break;
+    case "contato":
+      console.log("contato");
+      break;
+  }
+});
+
+//Forms
+//Puxando o formulário
+const form1 = document.forms.namedItem("registration");
+
+form1.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //Pegando dados do formulário
+  const name = form1.name.value;
+  const password = form1.password.value;
+  if (!name && !password) alert("Preencha nome e password");
+
+  //Criando o formData
+  const formData = new FormData(form1);
+  console.log(formData.get("name"), formData.get("password"));
+
+  //Criando elementos reativos no formulario
+  const newP = document.createElement("p");
+  newP.innerHTML = `Olá! <strong>${formData.get("name")}</strong>`;
+  form1.after(newP);
+});
+
+const form2 = document.forms.namedItem("radio-button");
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
